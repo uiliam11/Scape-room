50d825bdf65703207ac016951db129951df0d091
uiliam11 <100007488.joan23@fje.edu>
2021-01-26 22:40:10 +0100

    subida 26/01

--- js/escape.js
+++
@@ -1,9 +0,0 @@
-function val_1() {
-    var a = document.getElementById('val-1').value
-    if (a == 'A') {
-        return true
-    } else {
-        alert('')
-        return false
-    }
-}
---
+++ js/umateu.js
@@ -0,0 +1,21 @@
+//pagina4
+function val_1() {
+    var a = document.getElementById('val-1').value
+    if (a == 'cogito ergo sum') {
+        return true
+    } else {
+        alert('No parece suceder nada')
+        return false
+    }
+}
+
+//pagina5
+function val_2() {
+    var a = document.getElementById('val-2').value
+    if (a == '103') {
+        return true
+    } else {
+        alert('No es la combinación adecuada')
+        return false
+    }
+}
