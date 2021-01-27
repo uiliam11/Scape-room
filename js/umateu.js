bda97050d36171749c654af64825f2ebdaf7fda6
uiliam11 <100007488.joan23@fje.edu>
2021-01-27 13:27:35 +0100

    subida 27/01

--- js/umateu.js
+++ js/umateu.js
@@ -12,10 +12,32 @@ function val_1() {
 //pagina5
 function val_2() {
     var a = document.getElementById('val-2').value
-    if (a == '103') {
+    if (a == '121') {
         return true
     } else {
         alert('No es la combinación adecuada')
+        return false
+    }
+}
+//pagina6
+function val_3() {
+    var a = document.getElementById('val-3').value
+    if (a == '2021') {
+        return true
+    } else {
+        alert('Los hombres cerdo se rien al verte fallar')
+        return false
+    }
+}
+//pagina7
+function val_4() {
+    var a = document.getElementById('val-4').value
+    if (a == 'noveno círculo') {
+        return true
+    } else if (a == 'traicion') {
+        return true
+    } else {
+        alert('Los hombres cerdo se rien al verte fallar')
         return false
     }
 }
