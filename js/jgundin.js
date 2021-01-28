aae8f38ddc444f9d47bf0e64469c2f6157f19e69
uiliam11 <100007488.joan23@fje.edu>
2021-01-28 12:05:18 +0100

    subida 28/01

---
+++ js/jgundin.js
@@ -0,0 +1,9 @@
+function val_1() {
+    var a = document.getElementById('val-1').value
+    if (a == 'siglo 20') {
+        return true
+    } else {
+        alert('El angel caido te mira fijamente en desacuerdo')
+        return false
+    }
+}
